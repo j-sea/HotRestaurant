a3f5e351df08bad18e7557b4e980a65c8a3fe81b
Jonathan C <jsea@uw.edu>
2019-10-02 14:28:32 -0700

    synced route names

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/table", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function(req, res) {
