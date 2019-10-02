209207dd0a1d29fed60b3f95d222d205ac976c7c
Gabe <gabriel.eyer007@gmail.com>
2019-10-02 11:51:42 -0700

    Just Getting Started

---
+++ server.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
